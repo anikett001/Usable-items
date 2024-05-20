4d082dc985742fad658d163ff8691a8f67dd9473
Aniket tyagi <121341144+anikett001@users.noreply.github.com>
2024-05-20 19:08:44 +0530

    user Dashboard pages created
    added country number code in form

---
+++ Shigr/js/passShowHide.js
@@ -0,0 +1,12 @@
+var a;
+function pass(){
+if(a == 1){
+    document.getElementById("password").type="password";
+    document.getElementById("pass-icon").src="../../Login-signup page/Images/eye-hide-icon.png";
+    a= 0;
+}else{
+    document.getElementById("password").type="text";
+    document.getElementById("pass-icon").src="../../Login-signup page/Images/eye-show-icon.png";
+    a= 1;
+}
+}
